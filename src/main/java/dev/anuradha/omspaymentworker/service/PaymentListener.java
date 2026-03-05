68e3c955f17b26eaca59858361948aa57cab6cd7
Anuradha Belgaonkar <annubelgaonkar@gmail.com>
2026-03-05 18:47:44 +0530

    Added payment listener class

--- src/main/java/dev/anuradha/omspaymentworker/service/PaymentListener.java
+++ src/main/java/dev/anuradha/omspaymentworker/service/PaymentListener.java
@@ -19,17 +19,27 @@ public class PaymentListener {
     @SqsListener("${sqs.order-created-queue}")
     public void listen(OrderCreatedEvent event){
 
-       log.info("Received payment for order {}", event.getOrderId());
+        log.info("Processing payment for order {}", event.getOrderId());
 
-       //Simulate payment success
+        boolean paymentSuccess = Math.random() > 0.5;
+
         Order order = orderRepository.findById(event.getOrderId())
-                        .orElseThrow();
+                .orElse(null);
 
-        order.setStatus(OrderStatus.PAID);
+        if(order == null){
+            log.warn("Order {} not found yet. Will retry.", event.getOrderId());
+            throw new RuntimeException("Order not found yet");
+        }
+
+        if (paymentSuccess) {
+            order.setStatus(OrderStatus.PAID);
+            log.info("Payment SUCCESS for order {}", order.getId());
+        } else {
+            order.setStatus(OrderStatus.FAILED);
+            log.warn("Payment FAILED for order {}", order.getId());
+        }
 
         orderRepository.save(order);
-
-        log.info("Order {} marked as PAID", event.getOrderId());
 
     }
 }
